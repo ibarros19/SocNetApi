7b929c984616a360a600c825dd7ef5426637ed36
ivibarros <ivibarros19@gmail.com>
2022-02-26 11:35:36 -0700

    restarting to push

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/social-network-api-DB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-module.exports = mongoose.connection;
